0c698cb0629fac54892f59c98ab96627522120e3
GuillemoL99 <guillermo.lee@gmail.com>
2026-04-05 03:41:07 -0300

    borrado

--- src/models/GuestList.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const guestListSchema = new mongoose.Schema({
-  eventDate: { type: Date, required: true },
-  createdBy: { type: String, required: true }, // username del creador
-}, { timestamps: true });
-
-const GuestList = mongoose.model("GuestList", guestListSchema);
-export default GuestList;
--- src/models/ListSettings.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const listSettingsSchema = new mongoose.Schema({
-  key: { type: String, unique: true, default: "free_list" },
-  eventDate: { type: Date, default: null },
-}, { timestamps: true });
-
-const ListSettings = mongoose.model("ListSettings", listSettingsSchema);
-export default ListSettings;
